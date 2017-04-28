b4e5193ca3db6853e40ce7c5d508d3b9525d999e
Jorge <jorge@localhost>
2017-04-28 11:43:49 -0300

    read serial input to set motor speed & direction

--- arduino-stepper.ino
+++ arduino-stepper.ino
@@ -5,12 +5,14 @@ int STEPS = 11;
 int DIRECTION = 9;
 
 void setup() {
+  Serial.begin(57600);
   pinMode(STEPS, OUTPUT);
   pinMode(DIRECTION, OUTPUT);
+  digitalWrite(DIRECTION, LOW);
 }
 
 void loop() {
-  digitalWrite(DIRECTION, HIGH);
+  parseSerialInput();
   turn();
 }
 
@@ -19,5 +21,28 @@ void turn() {
     digitalWrite(STEPS, HIGH);
     digitalWrite(STEPS, LOW);
     delayMicroseconds(speed);
+  }
+}
+
+void parseSerialInput() {
+  if (Serial.available() > 0) {
+    String str = Serial.readString();
+    int separatorIndex = str.indexOf(':');
+    if (separatorIndex <= 0)
+      return;
+
+    String command = str.substring(0, separatorIndex);
+    int value = str.substring(separatorIndex + 1).toInt();
+
+    switch (command[0]) {
+      case 's':
+        speed = value;
+        break;
+      case 'd':
+        digitalWrite(DIRECTION, value);
+        break;
+      default:
+        break;
+    }
   }
 }
